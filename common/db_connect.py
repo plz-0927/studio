783f8b77b141a2eeb3050bc593aa0e22af0136f0
plz <plz0927@163.com>
2022-07-25 12:03:48 +0800

    更新了db_connect

--- common/db_connect.py
+++ common/db_connect.py
@@ -4,21 +4,24 @@
 # @Author: Russ
 
 import pymysql
-from common.log_handler import logger
+# from common.read_conf import ReadConfig
 
 
-class MysqlConnect:
-    logger = logger
+class MysqlDb:
 
-    def __init__(self, host, username, password, db, port, charset):
-        try:
-            self.conn = pymysql.connect(host=host, user=username, password=password, database=db,
-                                        port=port, charset=charset)
-            self.cur = self.conn.cursor()
-        except Exception as e:
-            logger.info("Database {0} connection failed".format(e))
+    def __init__(self, host, port, user, passwd, db):
+        # 建立数据库连接
+        self.conn = pymysql.connect(
+            host=host,
+            port=int(port),
+            user=user,
+            passwd=passwd,
+            db=db,
+            autocommit=True
+        )
+        # 通过 cursor() 创建游标对象，并让查询结果以字典格式输出cursor=pymysql.cursors.DictCursor
+        self.cur = self.conn.cursor()
 
-    # 关闭数据库连接
     def __del__(self):  # 对象资源被释放时触发，在对象即将被删除时的最后操作
         # 关闭游标
         self.cur.close()
@@ -31,7 +34,7 @@ class MysqlConnect:
         self.conn.ping(reconnect=True)
         # 使用 execute() 执行sql
         self.cur.execute(sql)
-        # 使用fetchall() 获取查询结果
+        # 使用 fetchall() 获取查询结果
         data = self.cur.fetchall()
         return data
 
@@ -48,3 +51,14 @@ class MysqlConnect:
             print("操作出现错误：{}".format(e))
             # 回滚所有更改
             self.conn.rollback()
+
+# if __name__ == '__main__':
+#     data_ = ReadConfig().read_ini('env.ini')
+#     host_ = data_.get('mysql','HOST')
+#     username_ = data_.get('mysql', 'USERNAME')
+#     password_ = data_.get('mysql', 'PASSWORD')
+#     db_ = data_.get('mysql', 'DB')
+#     port_ = data_.get('mysql', 'PORT')
+#     co = MysqlDb(host=host_, port=port_, user=username_, passwd=password_, db=db_)
+#     data = co.select_db('select * from user')
+#     print(data)
--- common/read_conf.py
+++ common/read_conf.py
@@ -2,8 +2,9 @@
 # !/usr/bin/python3.7
 # @Email:plz0927@163.com
 # @Author: Russ
+from configparser import ConfigParser
+
 import yaml
-from common.log_handler import logger
 
 """
 读取配置文件类
@@ -11,12 +12,16 @@ from common.log_handler import logger
 
 
 class ReadConfig:
-
-    logger = logger
-
     def __init__(self):
-        with open('../config/config.yml', 'r', encoding='utf-8') as f:
-            self.data = yaml.load(f, Loader=yaml.FullLoader)
+        self.path_dir = '../config/'
 
-    def read_info(self, point, info):
-        return self.data[point][info]
+    def read_config(self, config_filename):
+        with open(self.path_dir + config_filename, 'r', encoding='utf-8') as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        return data
+
+    def read_ini(self, ini_filename):
+        data = ConfigParser()
+        data.read(self.path_dir + ini_filename)
+        return data
+
